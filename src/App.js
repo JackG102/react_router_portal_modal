98b11b2f166eecd2c9d932e8153996dbc24009f9
Jack Garratt <jack.garratt@icfnext.com>
2021-11-20 18:54:04 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import Header from './components/Header';
 
 const App = () => {
   return(
     <>
       <Header />
+      <Switch>
+          <Route path="/about">
+            <h2>About</h2>
+          </Route>
+          <Route path="/users">
+            <h2>Users</h2>
+          </Route>
+          <Route path="/">
+            <h2>Home</h2>
+          </Route>
+        </Switch>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Header.css';
+import {
+  BrowserRouter as Router,
+  Link
+} from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className="header-container">
       <h2>Header</h2>
+      <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
     </div>
   );
 }
